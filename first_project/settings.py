17b53c686b28335ebe5b2aecc55783b4fbd9aea5
GURUCHARANTEJA3 <bunnyparamesugalla@gmail.com>
2026-09-07 16:49:31 +0530

    Giving Access to Render

--- first_project/settings.py
+++ first_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u&j9r+m#pa6j^b1i7im1l49a=ctv1i2p&&x7!mu32+j+e(d=!e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
